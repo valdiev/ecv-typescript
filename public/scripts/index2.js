c20c328f52cda40daf7826c9fd7ce17f37f280d7
valdiev <valdiev@live.fr>
2021-11-18 10:15:28 +0100

    Initialisation  du projet TypeScript

---
+++ public/scripts/index2.js
@@ -0,0 +1,6 @@
+"use strict";
+let arrayNumber = [];
+for (let i = 0; i < 10; i++) {
+    arrayNumber.push(Math.round(Math.random() * 10));
+}
+//# sourceMappingURL=index2.js.map
---
+++ src/index2.ts
@@ -0,0 +1,5 @@
+let arrayNumber: number[] = []
+
+for(let i=0; i<10; i++) {
+    arrayNumber.push(Math.round(Math.random() * 10))
+}
